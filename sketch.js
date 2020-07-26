f2e930dcd1da34d3d27bf510017ecbee9908a89c
dev528 <63849974+dev528@users.noreply.github.com>
2020-07-26 17:34:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var fixedRect, movingRect;
+var object1,object2
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
+
+  object1=createSprite(400,200,50,80)
+  object1.velocityY=5
+}
+
+function draw() {
+  background(0,0,0);  
+
+  bounceoff(movingRect,object1);
+  bounceoff(fixedRect,object1)
+  drawSprites();
+}
+
